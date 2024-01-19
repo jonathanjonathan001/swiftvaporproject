91bf137ff8a9797bf6b04c2383a3b25c72d6dd07
Jonathan Råsmark <jonathanrosemark@gmail.com>
2024-01-19 19:16:47 +0100

    Initial commit

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -3,8 +3,19 @@ import Vapor
 // configures your application
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
+    //app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+  
+    let fileMiddleware = FileMiddleware(
+        publicDirectory: app.directory.publicDirectory
+    )
+    app.middleware.use(fileMiddleware)
+    
+    
     // register routes
     try routes(app)
+    
+    
+    
 }
+
+
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,4 +8,5 @@ func routes(_ app: Application) throws {
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
+    
 }
